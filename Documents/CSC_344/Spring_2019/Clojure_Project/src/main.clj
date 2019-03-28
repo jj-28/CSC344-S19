94cf51b6f9362a4622cf60d963b4ce1722caa7ee
unknown <jjohns28@oswego.edu>
2019-03-28 08:44:16 -0400

    clojure finished

--- Documents/CSC_344/Spring_2019/Clojure_Project/src/main.clj
+++ Documents/CSC_344/Spring_2019/Clojure_Project/src/main.clj
@@ -1,5 +1,5 @@
 (ns main)
-
+;remove trues with
 
 ;(nand false) =: true checkkk
 ;(nand true) =: false checkkk
@@ -17,70 +17,72 @@
 ;(nand x y z) =: (nand x y z) checkkk
 ;
 ;change this to nand-convert, because there's only certai situations in which nand will happen
-
+;remove all the false
+;(defn simplify [q-list]
+;  (cond
+;    (and (= 2 (count q-list)) (false? (second q-list))) true
+;    (and (= 2 (count q-list)) (true? (second q-list))) false
+;    (and (seq? (second q-list)) (= (first q-list) (first (second q-list)))) (second (second q-list))
+;    (and (seq? (nth q-list 2)) (= (second q-list) (second (nth q-list 2)))) true
+;    (and (every? symbol? q-list) (not (apply distinct? (rest q-list)))) (distinct q-list)
+;    (every? symbol? (rest q-list)) q-list
+;    (and (some symbol? (rest q-list)) (some true? (rest q-list))) (remove boolean? q-list)
+;    (and (some symbol? (rest q-list)) (some false? (rest q-list))) true
+;    (and (every? boolean? (rest q-list)) (every? true? (rest q-list))) false
+;    :default q-list
+;
+;    )
+;  )
 (defn simplify [q-list]
   (cond
-    (and (= 2 (count q-list)) (false? (second q-list))) true
-    (and (= 2 (count q-list)) (true? (second q-list))) false
-    (and (seq? (second q-list)) (= (first q-list) (first (second q-list)))) (second (second q-list))
-    (and (seq? (nth q-list 2)) (= (second q-list) (second (nth q-list 2)))) true
-    (and (every? symbol? q-list) (not (apply distinct? (rest q-list)))) (distinct q-list)
-    (every? symbol? (rest q-list)) q-list
+    (some false? (remove symbol? q-list) ) true
     (and (some symbol? (rest q-list)) (some true? (rest q-list))) (remove boolean? q-list)
-    (and (some symbol? (rest q-list)) (some false? (rest q-list))) true
+    (and (every? symbol? q-list) (not (apply distinct? (rest q-list)))) (distinct q-list)
     (and (every? boolean? (rest q-list)) (every? true? (rest q-list))) false
-    :default "ya done goofed"
+    (and (seq? (second q-list)) (= (first q-list) (first (second q-list)))) (second (second q-list))
+    (and (seq? (nth q-list 2 "nothing found")) (= (second q-list) (second (nth q-list 2 "nothing found")))) true
+    :default q-list
 
     )
 
   )
 (defn app [t]
   (map (fn [i]
-          (conj (list  i) (symbol "nand"))
+         (conj (list i) (symbol "nand"))
          ) t)
   )
-
-(defn and-to-nand [q]
-  (= (symbol "and") (first q))  (conj (rest q) (symbol "nand") )
-
-  )
-(defn or-to-nand [q]
-  (= (symbol "or") (first q))  (cons (symbol "nand") (app (rest q)))
-  )
-
 (defn nand-convert [q]
-    (cond
-      ;not->nand
-      (= 1 (count (rest q))) (conj (rest q) (symbol "nand"))
-      (= (symbol "and") (first q)) (conj (rest q) (symbol "nand"))
-      ;or->nand
-      (= (symbol "or") (first q)) (cons (symbol "nand") (app (rest q)))
-      :default q )
+  (cond
+    ;not->nand
+    (= 1 (count (rest q))) (conj (rest q) (symbol "nand"))
+
+    (= (symbol "and") (first q)) (conj (list (cons (symbol "nand") (rest q))) (symbol "nand"))
+    ;or->nand
+
+    (= (symbol "or") (first q)) (cons (symbol "nand") (app (rest q)))
+    :default q)
 
   )
 (defn lookup [i m]
   (get m i i))
 
 (defn nested [l]
-  (simplify(map (fn [i]
-                      (if (seq? i)
-                        (con i )
-                        i
-                        ))
-                    l))
+  (simplify (map (fn [i]
+                   (if (seq? i)
+                     (nested i)
+                     i
+                     ))
+                 l))
   )
-
-(defn con[l ]
-  (nand-convert(map (fn [i]
-         (if (seq? i)
-           (con i )
-          i
-           ))
-       l))
+(defn com [l]
+  (nand-convert (map (fn [i]
+                       (if (seq? i)
+                         (com i)
+                         i
+                         ))
+                     l))
 
   )
-
-
 (defn bind-values [l m]
   (map (fn [i]
          (if (seq? i)
@@ -91,17 +93,14 @@
 
   )
 
+;(defn evalexp [exp bindings]
+;  (simplify (nand-convert (bind-values bindings exp))))
+
+
 (defn evalexp [exp bindings]
-  (simplify (nand-convert (bind-values bindings exp))))
-
-
-
-(def p4 '(and x (not z )))
+  (nested (com (bind-values  exp bindings)))
+  )
 
 (def p1 '(and x (or x (and y (not z)))))
 (def p2 '(and (and z false) (or x true false)))
 (def p3 '(or true a))
-
-;(defn simplify [uneval-list]
-;  (apply (resolve (symbol (first uneval-list))) [uneval-list])
-;  )
